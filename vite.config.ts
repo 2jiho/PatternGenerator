e0eae9659dcc9816b44c2983882d463a031ff3db
Jiho Lee <54468974+2jiho@users.noreply.github.com>
2025-02-24 22:26:48 +0900

    chore: 중복되는 base 설정 제거

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/PatternGenerator' : '/',
   plugins: [tailwindcss(), sveltekit()],
 });
