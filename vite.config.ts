5bbf02ab238d3e659043c24c7c93706ca7e6d490
Jiho Lee <54468974+2jiho@users.noreply.github.com>
2025-02-25 22:08:02 +0900

    chore: 포맷터 적용

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,16 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vitest/config";
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss(), sveltekit(), basicSsl()],
   test: {
-    environment: 'jsdom'
+    environment: "jsdom",
   },
   resolve: process.env.VITEST
     ? {
-      conditions: ['browser']
-    }
-    : undefined
+        conditions: ["browser"],
+      }
+    : undefined,
 });
