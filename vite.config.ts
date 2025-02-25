26ea7a6e749d0251df6058905a1487ffa4d57f50
Jiho Lee <54468974+2jiho@users.noreply.github.com>
2025-02-25 15:18:11 +0900

    test(init): 테스트 환경 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss(), sveltekit()],
+  test: {
+    environment: 'jsdom'
+  },
+  resolve: process.env.VITEST
+    ? {
+      conditions: ['browser']
+    }
+    : undefined
 });
