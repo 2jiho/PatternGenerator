5bc5950b43b7143c6bf2ab3b7592eaeb2d26fe5b
Jiho Lee <54468974+2jiho@users.noreply.github.com>
2025-02-25 15:45:57 +0900

    refactor: 슬라이더 스타일 재설정

--- src/lib/components/Slider.svelte
+++ src/lib/components/Slider.svelte
@@ -3,8 +3,18 @@
   const uid = $props.id();
 </script>
 
-<div class="flex justify-between">
-  <label for="{uid}-slider">{label}</label>
-  <label for="{uid}-slider">{value}{unit}</label>
-</div>
-<input id="{uid}-slider" type="range" bind:value {...others} />
+<main class="flex flex-col gap-2">
+  <div class="flex justify-between">
+    <label for="{uid}-slider">{label}</label>
+    <label for="{uid}-slider">{value}{unit}</label>
+  </div>
+  <div class="relative">
+    <input
+      id="{uid}-slider"
+      type="range"
+      bind:value
+      {...others}
+      class="focus:ring-opacity-50 h-2 w-full cursor-pointer appearance-none rounded-lg bg-gray-500 accent-blue-600 hover:bg-gray-300 hover:accent-blue-700 focus:ring-2 focus:ring-blue-500 focus:outline-none"
+    />
+  </div>
+</main>
