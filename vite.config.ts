ab9bb0582c9bae14cd6ba432e17e0094db71de7a
Jiho Lee <54468974+2jiho@users.noreply.github.com>
2025-03-04 00:58:55 +0900

    chore: 테스트 코드 제거

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,8 @@
 import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 import basicSsl from "@vitejs/plugin-basic-ssl";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [tailwindcss(), sveltekit(), basicSsl()],
-  test: {
-    environment: "jsdom",
-  },
-  resolve: process.env.VITEST
-    ? {
-        conditions: ["browser"],
-      }
-    : undefined,
 });
