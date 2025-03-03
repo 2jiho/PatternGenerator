fe08ebd222c781b60c5aaa686a73bbd5146fa292
Jiho Lee <54468974+2jiho@users.noreply.github.com>
2025-03-04 01:26:21 +0900

    fix: 포커스시 아웃라인 삭제

--- src/lib/components/PatternSelector.svelte
+++ src/lib/components/PatternSelector.svelte
@@ -5,7 +5,11 @@
 
 <div class="flex flex-col gap-2 bg-inherit">
   <label for="{uid}-select">Pattern Type</label>
-  <select id="{uid}-select" class="h-10 w-full cursor-pointer rounded-lg border-3 bg-inherit hover:bg-gray-700" bind:value={selectedPatternKey}>
+  <select
+    id="{uid}-select"
+    class="h-10 w-full cursor-pointer rounded-lg border-3 bg-inherit hover:bg-gray-700 focus:outline-none"
+    bind:value={selectedPatternKey}
+  >
     {#each Object.keys(PatternGenerators) as patternKey}
       <option value={patternKey}>
         {patternKey.charAt(0).toUpperCase() + patternKey.slice(1)}
