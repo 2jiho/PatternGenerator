fb2e8d8f42e0ec564fdd6c7b08e835e62f70796c
Jiho Lee <54468974+2jiho@users.noreply.github.com>
2025-03-04 00:26:19 +0900

    feat: 이미지 테두리 추가 및 패턴 셀렉터 컴포넌트화

---
+++ src/lib/components/ImageBorder.svelte
@@ -0,0 +1,73 @@
+<script lang="ts">
+  import Slider from "$lib/components/Slider.svelte";
+
+  let { imageBitmap, borderBitmap = $bindable() } = $props();
+  const uid = $props.id();
+
+  let borderWidthRatio: number = $state(0); // 테두리 두께 비율
+  let borderColor: string = $state("#ffffff"); // 테두리 색상
+
+  let debounceTimer: NodeJS.Timeout | undefined;
+  function debounce(func: () => void, delay: number = 200) {
+    clearTimeout(debounceTimer);
+    debounceTimer = setTimeout(func, delay);
+  }
+
+  async function drawBorderBitmap() {
+    if (!imageBitmap) return;
+
+    if (borderWidthRatio === 0) {
+      borderBitmap = imageBitmap;
+      return;
+    }
+
+    const imgW = imageBitmap.width;
+    const imgH = imageBitmap.height;
+    const borderWidth = Math.ceil((Math.max(imgW, imgH) * borderWidthRatio) / 100);
+
+    const canvasW = imgW + borderWidth * 2;
+    const canvasH = imgH + borderWidth * 2;
+    const canvas = new OffscreenCanvas(canvasW, canvasH);
+    const ctx = canvas.getContext("2d");
+
+    if (!ctx) return;
+
+    try {
+      ctx.shadowColor = borderColor;
+      ctx.shadowBlur = borderWidth;
+      ctx.shadowOffsetX = 0;
+      ctx.shadowOffsetY = 0;
+      ctx.drawImage(imageBitmap, borderWidth, borderWidth);
+
+      const imageData = ctx.getImageData(0, 0, canvasW, canvasH);
+      const data = imageData.data;
+
+      for (let i = 3; i < data.length; i += 4) {
+        data[i] = data[i] > 10 ? 255 : 0;
+      }
+
+      ctx.putImageData(imageData, 0, 0);
+      borderBitmap = await createImageBitmap(canvas);
+    } catch (error) {
+      console.error("Error drawing border:", error);
+      borderBitmap = imageBitmap;
+    }
+  }
+
+  const debouncedDrawBorderBitmap = () => {
+    debounce(drawBorderBitmap, 100);
+  };
+
+  $effect(() => {
+    const _ = [borderWidthRatio, borderColor, imageBitmap];
+    debouncedDrawBorderBitmap();
+  });
+</script>
+
+<div class="flex h-full w-full flex-col">
+  <Slider label="Border" unit="%" max={10} bind:value={borderWidthRatio} />
+  <div class="flex items-center gap-2">
+    <label for="{uid}-color">Color:</label>
+    <input type="color" id="{uid}-color" bind:value={borderColor} />
+  </div>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,14 +5,17 @@
   // 컴포넌트
   import Slider from "$lib/components/Slider.svelte";
   import ImageSelector from "$lib/components/ImageSelector.svelte";
+  import ImageBorder from "$lib/components/ImageBorder.svelte";
   import DownloadButton from "$lib/components/DownloadButton.svelte";
+  import PatternSelector from "$lib/components/PatternSelector.svelte";
 
   // 함수
   import PatternGenerators from "$lib/patternGenerator";
 
-  let imageBitmap: ImageBitmap;
+  let imageBitmap: ImageBitmap | undefined;
+  let borderBitmap: ImageBitmap | undefined;
   let patternImageBitmap: ImageBitmap | undefined;
-  let patternCanvas: HTMLCanvasElement;
+  let patternCanvas: HTMLCanvasElement | undefined;
 
   type PatternKey = keyof typeof PatternGenerators;
   let selectedPatternKey: PatternKey | undefined;
@@ -46,8 +49,8 @@
 
   $: {
     (async () => {
-      if (imageBitmap && selectedGenerator) {
-        patternImageBitmap = await selectedGenerator.generate(imageBitmap, paramValues);
+      if (imageBitmap && borderBitmap && selectedGenerator) {
+        patternImageBitmap = await selectedGenerator.generate(borderBitmap, paramValues);
       } else {
         patternImageBitmap = undefined;
       }
@@ -64,28 +67,17 @@
   <!-- 메뉴 시작 -->
   <div class="w-full bg-gray-900 text-white md:relative md:h-screen md:w-64">
     <div class="line-clamp-1 p-2 text-lg font-bold">Image Pattern Generator</div>
-    <div class="flex flex-col gap-2 p-2">
+    <div class="flex flex-col gap-2 bg-inherit p-2">
       <ImageSelector bind:imageBitmap />
-      <!-- 패턴 선택기 -->
-      <div class="mb-2">
-        <label for="pattern-select" class="mb-1 block text-sm">패턴 타입</label>
-        <select id="pattern-select" class="w-full rounded bg-gray-700 px-2 py-1 text-white" bind:value={selectedPatternKey}>
-          {#each Object.keys(PatternGenerators) as patternKey}
-            <option value={patternKey}>
-              {patternKey.charAt(0).toUpperCase() + patternKey.slice(1)}
-            </option>
-          {/each}
-        </select>
-      </div>
-      <!-- 동적 슬라이더 생성 -->
+      <ImageBorder {imageBitmap} bind:borderBitmap />
+      <PatternSelector bind:selectedPatternKey {PatternGenerators} />
+      <!-- 슬라이더 생성 -->
       {#if patternParams}
         {#each patternParams as param (param.id)}
           <Slider bind:value={paramValues[param.id]} min={param.min} max={param.max} step={param.step} unit={param.unit} label={param.label} />
         {/each}
       {/if}
       <Slider bind:value={paramValues["canvasWidth"]} min="100" max="4000" step="100" unit="px" label="Canvas Width" />
-    </div>
-    <div class="flex flex-col p-2 md:absolute md:bottom-4 md:w-64">
       <DownloadButton {patternImageBitmap} download={fileName} />
     </div>
   </div>
